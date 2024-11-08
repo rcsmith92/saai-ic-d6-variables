9b63d14da7e7130d373c69e0dde36012914e434f
Ryan Smith <rcsmith92@yahoo.com>
2024-11-08 04:11:44 +0000

    wrote first lines of JS

---
+++ script.js
@@ -0,0 +1,23 @@
+let animal = "dog"
+
+console.log("Awesome I'm now writing JavaScript!")
+console.log("2nd line of JS")
+console.log(animal)
+console.log("I'm still here..")
+
+//Examples
+
+//String
+let personName = "Ryan"
+console.log("persons name", personName)
+
+//Number
+let purchasePrice = 14.99
+console.log("initial value", purchasePrice)
+
+purchasePrice = 9.99
+console.log("updated value purchasePrice", purchasePrice)
+
+//Boolean
+let isMember = true
+console.log(isMember)
